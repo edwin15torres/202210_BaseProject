ceeb5b259992ea39ed61be5004926f71f6577b71
Edwin Torres <ea.torresl1@uniandes.edu.co>
2022-05-01 22:31:57 -0500

    conectar el modulo funcional con el principal

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+// 2. Importar file del cafes.module.ts
+import { CafesModule } from './cafes/cafes.module';
 
 @NgModule({
   declarations: [
@@ -10,7 +12,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CafesModule // 2. clase del modulo
   ],
   providers: [],
   bootstrap: [AppComponent]
