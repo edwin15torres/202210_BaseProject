bbf718e6d4f656c148156cdd7370457b0ddd14f4
Edwin Torres <ea.torresl1@uniandes.edu.co>
2022-05-03 00:38:53 -0500

    Pruebas componente

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,33 +1,46 @@
+
+// COMPONENTE DE PRUEBAS:
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+//14. Testing Http y comppnente ---> Luego de Faker,  Crear un course.component.spec.ts
+import { HttpClientModule } from '@angular/common/http';
+import { CafesComponent } from './cafes/cafes.component';
+
+// 1-) Preparar
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [RouterTestingModule],
-      declarations: [AppComponent],
+      imports: [
+        RouterTestingModule,
+        HttpClientModule  // 14.1 mod. http
+
+      ],
+      declarations: [
+        AppComponent,
+        CafesComponent // 14.2 mod. course
+      ],
     }).compileComponents();
   });
 
+    // 2-)  y 3-) Ejecutar y validar
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'base-project'`, () => {
+  it(`should have as title 'Practica_1'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('base-project');
+    expect(app.title).toEqual('Practica_1');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain(
-      'base-project app is running!'
-    );
+    expect(compiled.querySelector('.content span')?.textContent).toContain('Practica_1 app is running!');
   });
 });
--- src/app/cafes/cafes.service.spec.ts
+++ src/app/cafes/cafes.service.spec.ts
@@ -1,15 +1,20 @@
-/* tslint:disable:no-unused-variable */
-
+// COMPONENTE DE PRUEBAS:
 import { TestBed, async, inject } from '@angular/core/testing';
 import { CafesService } from './cafes.service';
+// 13. Testing Http -->aap.component.spect.ts
+import { HttpClientTestingModule } from "@angular/common/http/testing";
 
-describe('Service: Cafes', () => {
+// 1-) Preparar
+describe('Service: Course', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
+      // 13.1 import   --> ir test app.component.spect.ts
+      imports: [HttpClientTestingModule],
       providers: [CafesService]
     });
   });
 
+    // 2-)  y 3-) Ejecutar y validar
   it('should ...', inject([CafesService], (service: CafesService) => {
     expect(service).toBeTruthy();
   }));
